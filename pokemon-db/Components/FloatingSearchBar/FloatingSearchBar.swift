4352568e87adc751125ca350c0f614c5e2f47900
Andrés Villagómez Ríos <avillagomezr@cinepolis.com>
2021-07-05 16:19:31 -0500

    more changes

---
+++ pokemon-db/Components/FloatingSearchBar/FloatingSearchBar.swift
@@ -0,0 +1,20 @@
+//
+//  FloatingSearchBar.swift
+//  pokemon-db
+//
+//  Created by Admin on 05/07/21.
+//
+
+import UIKit
+
+class FloatingSearchBar: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
